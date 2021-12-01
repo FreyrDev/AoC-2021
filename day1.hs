8c46dff22f63d189b960fc7d8cd37e4f3d39ac92
Freyr <95385132+FreyrDev@users.noreply.github.com>
2021-12-01 22:39:22 +0000

    Day 1 Solution

---
+++ day1.hs
@@ -0,0 +1,16 @@
+stringToInts :: String -> [Int]
+stringToInts input = map read (words input)
+
+day1_1 :: [Int] -> Int
+day1_1 []     = 0
+day1_1 (x:xs) = sum $ zipWith (\x y -> if x < y then 1 else 0) (x:xs) xs
+
+day1_2 :: [Int] -> Int
+day1_2 []       = 0
+day1_2 [_]      = 0
+day1_2 (x:y:xs) = day1_1 $ zipWith3 (\x y z -> x + y + z) (x:y:xs) (y:xs) xs
+
+main :: IO ()
+main = do
+    input <- readFile "inputs/day1.txt"
+    print . day1_2 . stringToInts $ input
