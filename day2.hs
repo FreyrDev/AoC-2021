6cc3cec19a8033d14946af48fd21260c06afd797
Freyr <95385132+FreyrDev@users.noreply.github.com>
2021-12-02 11:52:37 +0000

    Day 2.2 Solution
    Solution for part 2 of day 2

--- day2.hs
+++ day2.hs
@@ -4,14 +4,22 @@ parse :: String -> [(Char,Int)]
 parse input = map (\x -> (head x, digitToInt (last x))) (lines input)
 
 day2_1 :: [(Char,Int)] -> Int
-day2_1 input = uncurry (*) $ go (0,0) input
+day2_1 = uncurry (*) . go (0,0)
     where go (x,y) [] = (x,y)
           go (x,y) (z:zs) | fst z == 'f' = go (x + snd z, y) zs
                           | fst z == 'u' = go (x, y - snd z) zs
                           | fst z == 'd' = go (x, y + snd z) zs
                           | otherwise    = (0,0)
 
+day2_2 :: [(Char,Int)] -> Int
+day2_2 = uncurry (*) . go 0 (0,0)
+    where go a (x,y) [] = (x,y)
+          go a (x,y) (z:zs) | fst z == 'f' = go a (x + snd z, y + (a * snd z)) zs
+                            | fst z == 'u' = go (a - snd z) (x,y) zs
+                            | fst z == 'd' = go (a + snd z) (x,y) zs
+                            | otherwise    = (0,0)
+
 main :: IO ()
 main = do
     input <- readFile "inputs/day2.txt"
-    print . day2_1 . parse $ input
+    print . day2_2 . parse $ input
