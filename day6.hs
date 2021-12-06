dabf8bdb178d36deea4d5630e11842a040384f40
Freyr <95385132+FreyrDev@users.noreply.github.com>
2021-12-06 18:37:49 +0000

    Alternate Day 6

--- day6.hs
+++ day6.hs
@@ -7,7 +7,7 @@ parse :: String -> [Integer]
 parse input = read ("[" ++ input ++ "]")
 
 merge :: [(Integer,Integer)] -> [(Integer,Integer)]
-merge = map (foldl1 (\(x,y) (a,b) -> (x,y+b))) . groupBy (on (==) fst) . sortBy (comparing fst)
+merge = map (foldl1 (\(x,y) (_,b) -> (x,y+b))) . groupBy (on (==) fst) . sortBy (comparing fst)
 
 day6 :: Integer -> [Integer] -> Integer
 day6 days input = go 0 (count input)
@@ -17,6 +17,14 @@ day6 days input = go 0 (count input)
           create (num,freq) = bool [(num,freq)] [(6,freq),(8,freq)] (num == (-1))
           shift (num,freq) = (pred num,freq)
           count = map (\x -> (head x, toInteger (length x))) . group . sort
+          
+day6V2 :: Integer -> [Integer] -> Integer
+day6V2 days input = go 0 (map (count input) [0,1,2,3,4,5,6,7,8])
+    where go n xs
+            | n == days = sum xs
+            | otherwise = go (succ n) (shift xs)
+          shift (x:xs) = take 6 xs ++ [xs !! 6 + x] ++ [xs !! 7] ++ [x]
+          count xs n = toInteger $ length (filter (==n) xs)
 
 main :: IO ()
 main = do
