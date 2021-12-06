a5446be731ed79d7eb5870586b85bdce3206cb31
Freyr <95385132+FreyrDev@users.noreply.github.com>
2021-12-06 21:29:16 +0000

    Modified Day 5

--- day5.hs
+++ day5.hs
@@ -1,29 +1,37 @@
+{-# LANGUAGE TupleSections #-}
 import Data.List (group, sort)
 
 parse :: String -> [((Int,Int),(Int,Int))]
 parse input = map ((\xs -> (toTuple (head xs), toTuple (last xs))) . words) $ lines input
     where toTuple x = read ("(" ++ x ++ ")")
 
+(...) :: (Ord a, Enum a) => a -> a -> [a]
+a ... b
+  | a <= b = [a .. b]
+  | otherwise = [a, pred a .. b]
+infixl 7 ...
+
+countCoords :: (Int -> Bool) -> [(Int,Int)] -> Int
+countCoords cond = length . filter cond . map length . group . sort
+
 day5_1 :: [((Int,Int),(Int,Int))] -> Int
-day5_1 = length . filter (>1) . map length . group . sort . concatMap (\((x1,y1),(x2,y2)) -> coords x1 y1 x2 y2) . filter orthogonal
-    where coords x1 y1 x2 y2 = if x1 == x2 then betweenY y1 y2 x1 else betweenX x1 x2 y1
-          betweenY p1 p2 other = [(other,y) | y <- [min p1 p2..max p1 p2]]
-          betweenX p1 p2 other = [(x,other) | x <- [min p1 p2..max p1 p2]]
+day5_1 = countCoords (>1) . concatMap (\((x1,y1),(x2,y2)) -> coords x1 y1 x2 y2) . filter orthogonal
+    where coords x1 y1 x2 y2 | x1 == x2  = (x1,) <$> y1...y2
+                             | y1 == y2  = (,y1) <$> x1...x2
+                             | otherwise = []
           orthogonal ((x1,y1),(x2,y2)) = x1 == x2 || y1 == y2
 
 day5_2 :: [((Int,Int),(Int,Int))] -> Int
-day5_2 = length . filter (>1) . map length . group . sort . concatMap (\((x1,y1),(x2,y2)) -> coords x1 y1 x2 y2) . filter straight
+day5_2 = countCoords (>1) . concatMap (\((x1,y1),(x2,y2)) -> coords x1 y1 x2 y2) . filter straight
    where coords x1 y1 x2 y2
-           | abs (x2 - x1) == abs (y2 - y1) = betweenDiagonal x1 y1 x2 y2
-           | x1 == x2               = betweenY y1 y2 x1
-           | otherwise               = betweenX x1 x2 y1
-         betweenY p1 p2 other = [(other,y) | y <- [min p1 p2..max p1 p2]]
-         betweenX p1 p2 other = [(x,other) | x <- [min p1 p2..max p1 p2]]
+           | y1 == y2  = (,y1) <$> x1 ... x2
+           | x1 == x2  = (x1,) <$> y1 ... y2
+           | otherwise = betweenDiagonal x1 y1 x2 y2
          betweenDiagonal x1 y1 x2 y2
-           | x2 > x1 && y2 > y1 = [(x,x+(y1-x1)) | x <- [x1..x2]]
-           | x1 > x2 && y2 > y1 = [(x,(y2+x2)-x) | x <- [x2..x1]]
-           | x1 > x2 && y1 > y2 = [(x,x+(y1-x1)) | x <- [x2..x1]]
-           | x2 > x1 && y1 > y2 = [(x,(y2+x2)-x) | x <- [x1..x2]]
+           | x2 > x1 && y2 > y1 = (\x -> (x, x + (y1-x1))) <$> x1...x2
+           | x1 > x2 && y2 > y1 = (\x -> (x, (y2+x2) - x)) <$> x1...x2
+           | x1 > x2 && y1 > y2 = (\x -> (x, x + (y1-x1))) <$> x1...x2
+           | x2 > x1 && y1 > y2 = (\x -> (x ,(y2+x2) - x)) <$> x1...x2
            | otherwise = [(x1,y1)]
          straight ((x1,y1),(x2,y2)) = x1 == x2 || y1 == y2 || (abs (x2 - x1) == abs (y2 - y1))
 
